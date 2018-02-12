5718aa4e363c3761d1fc657bf53af2b3a190ecdb
Stacey Borrego <staceyborrego@gmail.com>
2018-02-12 11:19:38 -0800

    update

--- directory_setup.sh
+++ directory_setup.sh
@@ -1,55 +1,69 @@
 #!/bin/bash
-set -euo pipefail
+
+# This tells the program to stop if it runs into any issues
+# -e Exit immediately when a command fails
+# -u Treats unset/unbound variables as an error and exits immediately
+# -x Prints each command before executing it -- helpful for debugging
+# -o Sets the exit code (0 is successful) to the that of the rightmost command...continued on next line
+# -o If the first command in a pipeline failed it will be carried through to the end of the pipeline and still exit instead of continuing through the script
+set -euxo pipefail
 
 # Change experiment name
 export EXP_NAME=MY_PROJECT
 # Change root dir if necessary
-export ROOT_DIR=/data/users/$USER/BioinformaticsSG/Getting-Ready-for-Analysis/
-export SUB_DIR=${ROOT_DIR}/RNASEQ
+export ROOT_DIR=/data/users/$USER/BioinformaticsSG
+# Change 
+export SUB_DIR=${ROOT_DIR}/Getting-Ready-for-Analysis
 # Change location of reference genome
-export REF_DIR=${ROOT_DIR}/ref_genomes
+export REF_DIR=${ROOT_DIR}/griffith_data/refs
 # Change to indexed reference genome.
-# Options: sacCer3, ercc92, hg38, hg38_ercc92
-export GENOME=hg38_ercc92
-export GTF=hg38_ercc92_anno
+export GENOME="22"
+export SPIKE="ERCC92"
 
+# These variables identify the paths for the genome reference and gene features files 
 export REF=${REF_DIR}/${GENOME}
-export REF_IDX=${REF}/${GENOME}
-export REF_GTF=${REF_DIR}/${GTF}/${GTF}.gtf
+export REF_FA=${REF}.fa
+export REF_GTF=${REF}.gtf
 
+# These variables identify the paths for the directories for your project result files
+# This is your project directory
 export EXP_DIR=${SUB_DIR}/${EXP_NAME}
-export ORG_DATA_DIR=${EXP_DIR}/original_data
+# This is the directory for your data
 export DATA_DIR=${EXP_DIR}/data
+# This is the directory for your alignment result files
 export ALIGNMENTS_DIR=${EXP_DIR}/alignments
-export FLAG_DIR=${ALIGNMENTS_DIR}/flag
+# This is the directory for your abundance estimation result files
 export COUNTS_DIR=${EXP_DIR}/counts
+# This is the directory for your differential expression (DE) analysis result files
 export DE_DIR=${EXP_DIR}/DE_analysis
 
-export FQOUT_DIR=${EXP_DIR}/fqout
-export FQHTML_DIR=${FQOUT_DIR}/fqhtml
-export FQHTML_TRIM_DIR=${FQOUT_DIR}/fqhtml_trim
+# This is the directory for your FastQC result files
+export QC_OUT_DIR=${EXP_DIR}/fastqc_out
+# This is the subdirectory for your HTML FastQC result files
+export QC_HTML_DIR=${FQOUT_DIR}/fastqc_html
 
+# This is the directory for your trimmed data
 export TRIM_DATA_DIR=${EXP_DIR}/trim_data
-export SE_ADAPTER_DIR=/home/stacey/miniconda3/pkgs/trimmomatic-0.36-5/share/trimmomatic-0.36-5/adapters/TruSeq2-SE.fa
-export PE_ADAPTER_DIR=/home/stacey/miniconda3/pkgs/trimmomatic-0.36-5/share/trimmomatic-0.36-5/adapters/TruSeq2-PE.fa
+# This is the directory for your alignment result files from your trimmed data
 export TRIM_ALIGNMENTS_DIR=${EXP_DIR}/trim_alignments
+# This is the directory for your abundance estimated result files from your trimmed data
 export TRIM_COUNTS_DIR=${EXP_DIR}/trim_counts
-export TRIM_FLAG_DIR=${TRIM_ALIGNMENTS_DIR}/trim_flag
+# Below is an example of setting a path to file that you may need for the program you are using.
+# These files are used to remove the adapters from the sequences. 
+# export SE_ADAPTER_DIR=/home/stacey/miniconda3/pkgs/trimmomatic-0.36-5/share/trimmomatic-0.36-5/adapters/TruSeq2-SE.fa
+# export PE_ADAPTER_DIR=/home/stacey/miniconda3/pkgs/trimmomatic-0.36-5/share/trimmomatic-0.36-5/adapters/TruSeq2-PE.fa
 
-export COUNT_OUT=${COUNTS_DIR}/counts
-export TRIM_COUNT_OUT=${TRIM_COUNTS_DIR}/trim_counts
-
+# Here we make the directories that may not already exist.
+# -p is a flag that tells the the mkdir program to ignore any warnings such as the directory already existing.
 mkdir -p ${DATA_DIR}
 mkdir -p ${ALIGNMENTS_DIR}
 mkdir -p ${COUNTS_DIR}
-mkdir -p ${FLAG_DIR}
 mkdir -p ${DE_DIR}
 
-mkdir -p ${FQOUT_DIR}
-mkdir -p ${FQHTML_DIR}
-mkdir -p ${FQHTML_TRIM_DIR}
+mkdir -p ${QC_OUT_DIR}
+mkdir -p ${QC_HTML_DIR}
 
 mkdir -p ${TRIM_DATA_DIR}
 mkdir -p ${TRIM_ALIGNMENTS_DIR}
 mkdir -p ${TRIM_COUNTS_DIR}
-mkdir -p ${TRIM_FLAG_DIR}
+
