169e3e786bcb4b6c8446e66383e5ea5cc55ab01d
Stacey Borrego <staceyborrego@gmail.com>
2018-02-12 20:11:17 -0800

    update 03

--- 03_combine_simple_files.sh
+++ 03_combine_simple_files.sh
@@ -14,6 +14,6 @@ echo "Run by `whoami` on `date`" > ${RUNLOG}
 for ID in `seq 1 4`; do
     FILE_ID=`printf %0.1d $ID`
     echo "*** Combo $FILE_ID is up!"
-    echo concatenate *${FILE_ID}* >> ${RUNLOG}
+    echo concatenate ${SIMPLE_DIR}/*${FILE_ID}* >> ${RUNLOG}
     cat ${SIMPLE_DIR}/*${FILE_ID}* > ${COMBO_SIMPLE_DIR}/combined_simple_file_${FILE_ID}.txt
 done
