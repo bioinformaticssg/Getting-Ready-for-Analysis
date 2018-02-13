5bac44afadc741a79d75af832379e88e67e9d5a8
Stacey Borrego <staceyborrego@gmail.com>
2018-02-12 19:55:06 -0800

    update make simple files

--- 02_make_simple_files.sh
+++ 02_make_simple_files.sh
@@ -6,12 +6,8 @@ SIMPLE_DIR=/data/users/$USER/BioinformaticsSG/Getting-Ready-for-Analysis/simple_
 
 mkdir -p ${SIMPLE_DIR}
 
-RUNLOG=${SIMPLE_DIR}/make_simple_files_runlog.txt
-echo "Run by `whoami` on `date`" > ${RUNLOG}
-
 for NUMBER in `seq 1 4`; do
 	for LETTER in A B; do
 		echo ${NUMBER}_${LETTER} > ${SIMPLE_DIR}/simple_file_${NUMBER}_${LETTER}.txt 2>> ${RUNLOG}
 	done
 done
-
---
+++ 03_combine_simple_files.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Exit this script on any error.
+set -euxo pipefail
+
+SIMPLE_DIR=/data/users/$USER/BioinformaticsSG/Getting-Ready-for-Analysis/simple_files
+
+RUNLOG=${SIMPLE_DIR}/simple_files_runlog.txt
+echo "Run by `whoami` on `date`" > ${RUNLOG}
+
+SAMPLES=${SAMPLE_DIR}/sample_list.txt
+echo "SAMPLE INFO" > ${SAMPLES}
+
+for ID in `seq 1 4`; do
+    FILE_ID=`printf %0.1d $ID`
+    echo "*** Combo $FILE_ID is up!"
+    echo concatenate ${ORG_DATA_DIR}/*${FILE_ID}* >> $RUNLOG
+    cat ${SIMPLE_DIR}/*${FILE_ID}* > ${SIMPLE_DIR}/combined_simple_file_${FILE_ID}.txt
+done
