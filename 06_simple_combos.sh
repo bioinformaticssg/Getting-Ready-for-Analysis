ac4e8100a221cd1a8388f58be8f2d0c08f56d52b
Stacey Borrego <staceyborrego@gmail.com>
2018-02-12 23:14:15 -0800

    update

--- 06_simple_combos.sh
+++ 06_simple_combos.sh
@@ -14,7 +14,7 @@ RUNLOG=${COMBO_SIMPLE_DIR}/simple_files_runlog.txt
 echo "Run by `whoami` on `date`" > ${RUNLOG}
 
 for ID in `seq $2 $3`; do
-    echo "*** Combo $FILE_ID is up!"
+    echo "*** Combo ${ID} is up!"
     echo concatenate ${SIMPLE_DIR}/*${FILE_ID}* >> ${RUNLOG}
     cat ${SIMPLE_DIR}/*${FILE_ID}* > ${COMBO_SIMPLE_DIR}/combined_simple_file_${FILE_ID}.txt
 done
