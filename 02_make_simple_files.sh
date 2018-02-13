da4c255e932e0b10f090f4abbf423a171607e33b
Stacey Borrego <staceyborrego@gmail.com>
2018-02-12 20:13:34 -0800

    update 02

--- 02_make_simple_files.sh
+++ 02_make_simple_files.sh
@@ -8,6 +8,6 @@ mkdir -p ${SIMPLE_DIR}
 
 for NUMBER in `seq 1 4`; do
 	for LETTER in A B; do
-		echo ${NUMBER}_${LETTER} > ${SIMPLE_DIR}/simple_file_${NUMBER}_${LETTER}.txt 2>> ${RUNLOG}
+		echo ${NUMBER}_${LETTER} > ${SIMPLE_DIR}/simple_file_${NUMBER}_${LETTER}.txt
 	done
 done
