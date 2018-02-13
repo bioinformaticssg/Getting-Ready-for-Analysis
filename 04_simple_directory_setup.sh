c1ffe9c659b3b1854dcf9d059522ba6775b3680d
Stacey Borrego <staceyborrego@gmail.com>
2018-02-12 23:06:43 -0800

    update

--- 04_simple_directory_setup.sh
+++ 04_simple_directory_setup.sh
@@ -4,9 +4,6 @@ export ROOT_SIMPLE_DIR=/data/users/$USER/BioinformaticsSG/Getting-Ready-for-Anal
 export SIMPLE_DIR=${ROOT_SIMPLE_DIR}/simple_files
 export COMBO_SIMPLE_DIR=${ROOT_SIMPLE_DIR}/combo_simple_files
 
-
-export RUNLOG=${SIMPLE_DIR}/runlog.txt
-
 mkdir -p ${ROOT_SIMPLE_DIR}
 mkdir -p ${SIMPLE_DIR}
 mkdir -p ${COMBO_SIMPLE_DIR}
